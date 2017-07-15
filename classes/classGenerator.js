76ae3ecd361b93ddbdb88debf204b995ae3e1b75
Tom Kent <email@tomkent.me>
2017-07-15 11:57:36 +0100

    Considerable code cleanup
    Implemented modinfo
    Code now runs as mini modules
    Game and mod data is loaded dynamically depending on the modinfo file

--- classes/classGenerator.js
+++ classes/classGenerator.js
@@ -13,11 +13,27 @@ var config = {
 };
 
 var staticconfig = {
-	typesfile: 'types.json',
-	typesoverridesfile: 'types_overrides.json'
+	mod: {
+		modinfo: 'modinfo.json',
+		typesoverrides: 'overrides' + path.sep + 'types_overrides.json'
+	},
+	gamedata: {
+		types: 'types.json',
+	}
+	
 };
 
-var data = {};
+var loadlist = {
+	mod: [],
+	gamedata: []
+};
+
+var data = {
+	mod: {},
+	gamedata: {}
+};
+
+var modinfo = {};
 
 function Generator(datadirectory, moddirectory, outdirectory) {
     config.datadir = datadirectory;
@@ -28,7 +44,7 @@ function Generator(datadirectory, moddirectory, outdirectory) {
 }
 
 method.getDirectory = function() {
-    return this._datadir;
+    return config.datadir;
 };
 
 
@@ -38,18 +54,33 @@ method.doRun = function() {
 	// Run each module in a series
 	async.series([
 
-		// run overrides first
-	    function(callback) {
-	        doOverrides();
+	    function(callback) { // load modinfo
+	        getModInfo();
+
+	        callback(null, 'modinfo');
+	    },
+	    function(callback) { // load json data
+	        initJSONData();
+
+	        callback(null, 'loaddata');
+	    },
+	    function(callback) { // actual logic
+	        doLogic();
 
 	        callback(null, 'overrides');
+	    },
+	    function(callback) { // save game data
+	        saveGameData();
+
+	        callback(null, 'saveata');
 	    }
 	],
 	// optional callback
 	function(err, results) {
 	    // results is now equal to ['one', 'two']
-	    console.log();
-	    console.log(chalk.bold.cyan("Code Generation Complete (ran " + results.length + " modules)"));
+	    console.log(chalk.bold.white(""));
+	    console.log(chalk.bold.white("Code Generation Complete (ran " + results.length + " tasks)"));
+	    console.log(chalk.bold.white(""));
 	});
 	
 	
@@ -57,21 +88,96 @@ method.doRun = function() {
 }
 
 
+// Get mod info from modinfo.json
+function getModInfo() {
+	modinfo = JSON.parse(fs.readFileSync(config.moddir + path.sep + staticconfig.mod.modinfo , 'utf8'));
+
+	outputModInfo();
+}
+
+// Tell the user what we're loading etc...
+function outputModInfo() {
+
+	console.log(chalk.bold.white("--------------------------------------------------------------------"));
+	console.log(chalk.bold.white("Loading mod: " + modinfo.name + ' (version: ' + modinfo.version + ')'));
+	console.log(chalk.bold.white("Authors: " + modinfo.authors));
+	console.log(chalk.bold.white("Running Modules: " + Object.keys(modinfo.modules).join(', ')));
+	console.log(chalk.bold.white("--------------------------------------------------------------------"));
+	console.log(chalk.bold.white(""));
+}
+
+// load all the required data
+function initJSONData() {
+	// tell the user what we're doing
+	console.log(chalk.bold.yellow("Initialising Mod"));
+
+	// first, build a list of tiles to load!
+	if(modinfo.modules.hasOwnProperty('typesoverrides')) {
+		loadlist.mod.push('typesoverrides');
+		loadlist.gamedata.push('types');
+	}
+
+	
+	loadJSONFiles();
+}
+
+// load individual files to our data structure
+function loadJSONFiles() {
+
+	// tell the user what we're doing
+	console.log(chalk.bold.yellow("Loading Mod & Game JSON"));
+
+	// load mods data
+	loadlist.mod.forEach(function(file) {
+	    data.mod[file] = JSON.parse(fs.readFileSync(config.moddir + path.sep + staticconfig.mod[file] , 'utf8'));
+	});
+
+	// load game data
+	loadlist.gamedata.forEach(function(file) {
+	    data.gamedata[file] = JSON.parse(fs.readFileSync(config.datadir + path.sep + staticconfig.gamedata[file] , 'utf8'));
+	});
+}
+
+// Do actual modding here
+function doLogic() {
+	
+	// tell the user what we're doing
+	console.log(chalk.bold.yellow("Starting Mod Loop"));
+
+	if(modinfo.modules.hasOwnProperty('typesoverrides')) {
+		doOverrides();
+	}
+}
+
+// Save out all game data!
+function saveGameData() {
+
+	// tell the user what we're doing
+	console.log(chalk.bold.yellow("Saving New Gamedata"));
+
+	// Go throughall the gamedata we loaded
+	loadlist.gamedata.forEach(function(file) {
+
+		// make it into a nice json string
+		var datastring = JSON.stringify(data.gamedata[file], null, 2);
+
+		// save it
+	    fs.writeFileSync(config.outdir + path.sep + staticconfig.gamedata[file], datastring); 
+
+	});
+}
+
+// Perform types overrides
 function doOverrides() {
-	// Tell the user we're starting
-	console.log(chalk.bold.green("Starting: Types Data Override!"));
 
-	// find types.json
-	data.types = JSON.parse(fs.readFileSync(config.datadir + path.sep + staticconfig.typesfile , 'utf8'));
-
-	// find types_overrides.json
-	data.typesoverrides = JSON.parse(fs.readFileSync(config.moddir + path.sep + 'overrides' + path.sep + staticconfig.typesoverridesfile , 'utf8'));
+	// tell the user what we're doing
+	console.log(chalk.bold.yellow("Performing Type Overrides"));
 
 	// Assume it worked, this time just parse to JSON and pass it straight to the merger
     performTypesOverrides();
 
-    // pass to the save function, with the number of changes
-	saveModifiedTypes();
+    // tell the user what we're doing
+	console.log(chalk.bold.yellow("Type Overrides Completed"));
 
 }
 
@@ -82,26 +188,13 @@ function performTypesOverrides() {
 	var target = {};
 
 	// cache the number of changes (to tell the user)
-	data.typesoverridescount = Object.keys(data.typesoverrides).length;
+	data.mod.typesoverridescount = Object.keys(data.mod.typesoverrides).length;
 
 	// merge the changes
-	extend(true, target, data.types, data.typesoverrides);
+	extend(true, target, data.gamedata.types, data.mod.typesoverrides);
 
-	data.types = target;
+	data.gamedata.types = target;
 
-}
-
-function saveModifiedTypes() {
-
-	// stringify the JSON, with pretty print
-	var typesfile = JSON.stringify(data.types, null, 2);
-
-	// write it out to the file
-	fs.writeFileSync(config.outdir + path.sep + staticconfig.typesfile, typesfile); 
-
-	// Let the user know what's going on    
-    console.log(chalk.bold.green("Complete: Types Data Overridden!"));
-    console.log(chalk.bold.yellow('Total changes made: ' + data.typesoverridescount));
 }
 
 module.exports = Generator;
